f9d72d2daf4b934170c87196d947026083f8ab89
jjguillen <jjavierguillen@gmail.com>
2019-12-17 19:00:55 +0100

    Cambios constructor
    Y añadidos comentarios

--- incidenciasmongo/Modelo/MIncidencia.php
+++ incidenciasmongo/Modelo/MIncidencia.php
@@ -7,8 +7,8 @@
     {
         protected $conexion=null;
 
-        //Abrir conexion a BD
-        function __construct($dbname="Ejercicio",$user="usuario",$password="iesjrs20") {
+        //Abrir conexion a BD MongoDB, cambiar nombre a la vuestra
+        function __construct($dbname="Ejercicio") {
             $this->conexion = (new MongoDB\Client)->{$dbname};
         }
 
@@ -18,10 +18,12 @@
 
         //Método de acceso a BD
         function getIncidencias() {
-
+            //incidencias es la colección (como una tabla) donde se consultan las incidencias
             $cursor = $this->conexion->incidencias->find();
             $incidencias = array();
             foreach($cursor as $incidencia) {
+                //Mongo nos devuelve un array asociativo con cada documento. 
+                //Generamos un objeto con esos valores para que funcione el resto de la app
                 $incidencia = new Incidencia($incidencia['Titulo'],$incidencia['Descripcion'],$incidencia['Fecha']);
                 $incidencias[] = $incidencia;
             }
